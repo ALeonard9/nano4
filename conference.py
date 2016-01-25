37c0b3c41d9c81e5e27d846e1792a93e4456ead1
Adam <LeoNineStudios@outlook.com>
2016-01-25 13:32:29 -0500

    Update conference.py

--- conference.py
+++ conference.py
@@ -179,6 +179,10 @@ class ConferenceApi(remote.Service):
         # create Conference, send email to organizer confirming
         # creation of Conference & return (modified) ConferenceForm
         Conference(**data).put()
+        taskqueue.add(params={'email': user.email(),
+            'conferenceInfo': repr(request)},
+            url='/tasks/send_confirmation_email'
+        )
         
         return request
 
